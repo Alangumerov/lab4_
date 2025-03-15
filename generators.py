a14e43e16a163a10baf07d02ac4fe59ab248c414
Alangumerov <a_gumerov@kbtu.kz>
2025-03-15 15:56:29 +0600

    Update generators.py

--- generators.py
+++ generators.py
@@ -1,4 +1,4 @@
-1)
+#1
 def generate(n):
     for i in range(1, n+1):
         yield i**2
@@ -8,7 +8,7 @@ for square in squares_generate:
     print(square)
 
 
-2)
+#2
 def even_numbers_generator(n):
     for i in range(0, n+1, 2):
         yield i
@@ -16,7 +16,7 @@ n = int(input())
 even_numbers = even_numbers_generator(n)
 print(", ".join(map(str, even_numbers)))
 
-3)
+#3
 def divisible_by_3_and_4(num):
     for i in range(0, num+1):
         if i % 12 == 0:
@@ -26,7 +26,7 @@ numbers = divisible_by_3_and_4(num)
 for number in numbers:
     print(number)
 
-4)
+#4
 
 def generate(a, b):
     for i in range(a, b+1):
@@ -38,7 +38,7 @@ for square in squares_generate:
     print(square)
 
 
-5) 
+#5
 def countdown(n):
     for i in range(n, -1, -1):
         yield i
